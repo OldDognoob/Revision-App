e5677784026feaf318286362d6c380900404c205
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-10 18:58:22 +0200

    create-hooks

---
+++ revision-app/src/hooks/container1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from '../routes';
+
+const HooksContainer1 = () => {
+
+    return(
+        <div>
+            React Hooks
+        </div>
+    )
+
+}
+export default HooksContainer1;
