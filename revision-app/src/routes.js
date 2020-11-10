2c3f7a4a62d6c7d4a800a31d8c4225e144da1092
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-10 19:14:46 +0200

    create-utils-files

--- revision-app/src/routes.js
+++ revision-app/src/routes.js
@@ -1,15 +1,15 @@
-import Component1 from './functional/component1';
-import Callback from './functional/callback';
-import PrivateComponent from './functional/privatecomponent';
-import UnauthRedirect from './functional/unauthredirect';
+import Component1 from './functional/Component1';
+import Callback from './functional/Callback';
+import PrivateComponent from './functional/PrivateComponent';
+import UnAuthRedirect from './functional/UnAuthRedirect';
 import Home from './functional/home';
-import RenderListItem from './functional/renderlistitem';
+import RenderListItem from './functional/RenderListItem';
 
 import * as ACTIONS from './store/actions/actions';
 import HooksContainer1 from './hooks/HooksContainer1';
 
-import Auth from './utils/auth';
-import AuthCheck from './utils/authcheck';
-import history from './utils/history';
+import Auth from './utils/Auth';
+import AuthCheck from './utils/AuthCheck';
+import History from './utils/History';
 
 import {Router, Route, Switch, Redirect} from 'react-router';
