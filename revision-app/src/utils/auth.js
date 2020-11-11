76bd39af2a7e2ead7c8fa8bdae4e3c950d1f8f99
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-11 18:41:39 +0200

    auth-create-file

--- revision-app/src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-import auth0 from 'auth0-js';
-import history from './history';
-
-export default class Auth {
-    auth0 = new auth0.WebAuth({
-        domain: 'webapp1.auth0.com',
-        clientID: 
-    })
-}
